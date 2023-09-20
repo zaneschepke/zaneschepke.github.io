47add1b27eba9121d3005d30bfc2b041e57a5ec6
Zane Schepke <zanecschepke@gmail.com>
2023-09-20 12:29:25 -0400

    initial commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,14 +8,14 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.mjs
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `Zane Schepke`,
+    siteTitleAlt: `Zane Schepke - Software Dev`,
+    siteHeadline: `Zane Schepke - Gatsby Theme from @lekoarts`,
+    siteUrl: `https://zaneschepke.github.io`,
+    siteDescription: `Zane Schepke's software developer site`,
     siteImage: `/banner.jpg`,
     siteLanguage: `en`,
-    author: `@lekoarts_de`,
+    author: `@zaneschepke`,
   },
   trailingSlash: `always`,
   plugins: [
